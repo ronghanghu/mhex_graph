f2dc930fedfb1e8c450c6050d8c62320e248522e
Ronghang Hu <huronghang@hotmail.com>
2015-02-09 12:14:53 -0800

    fix none-of-above

--- add_none_of_above.m
+++ add_none_of_above.m
@@ -1,9 +1,11 @@
-function synsets_augmented = add_none_of_above(synsets)
+function synsets_augmented = add_none_of_above(synsets, add_noa_to_root)
 % synsets_augmented = add_none_of_above(synsets)
 %   Adding a 'none-of-above' node to each branch node and root node. Such
 %   'none-of-above' nodes are added at the end of synsets.
 %
 %   synsets is the original (input) synsets
+%   add_noa_to_root flag decides whether or not to add a 'none-of-above' child
+%   to root node. default is false.
 %   synsets_augmented is the augmented (output) synsets with
 %   'none-of-above' nodes
 
@@ -17,12 +19,35 @@ function synsets_augmented = add_none_of_above(synsets)
 % this file (or any portion of it) in your project.
 % ---------------------------------------------------------
 
+if ~exist('add_noa_to_root', 'var')
+  add_noa_to_root = false;
+end
+
+% find root node (the node without parent)
+root = 0;
+if ~add_noa_to_root
+  is_root = true(length(synsets), 1);
+  for v = 1:length(synsets)
+    is_root(synsets(v).children) = false;
+  end
+  root = find(is_root);
+  assert(length(root) == 1);
+  fprintf('root synset is\n');
+  disp(synsets(root));
+end
+
 synsets_augmented = synsets;
 
+fprintf('adding none-of-above nodes...\n');
 count = 0;
 for v = 1:length(synsets)
   % for all branch nodes, add 'none-of-above' child
   if ~isempty(synsets(v).children)
+    if ~add_noa_to_root && (v == root)
+      fprintf('skipped root node\n');
+      continue
+    end
+
     % create new synset
     % replacing WNID from nxxxxxxxx to dxxxxxxx to represent 'dummy' synset
     noa_synset = synsets(v);
@@ -37,6 +62,6 @@ for v = 1:length(synsets)
   end
 end
 
-fprintf('added %d none-of-above children\n', count);
+fprintf('done. added %d none-of-above children\n', count);
 
 end
